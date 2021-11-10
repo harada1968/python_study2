b6cdb29e8ad60fc3fa4f874423260480ae02e19f
harakazu <info,harakazu@gmail.com>
2021-11-10 22:38:25 +0900

    検索まで

--- main.py
+++ main.py
@@ -13,7 +13,20 @@ driver.implicitly_wait(10)
 driver.get('https://tenshoku.mynavi.jp/')
 sleep(3)
 
-search_box = driver.find_element_by_css_selector('topSearch__item')
+a_tag = driver.find_element_by_css_selector('div._CloseButton__3xX2_ > button')
+sleep(3)
+
+a_tag.click()
+sleep(5)
+
+b_tag = driver.find_element_by_css_selector('a._karte-temp-btn-close__2u7Y_')
+sleep(3)
+
+b_tag.click()
+sleep(3)
+
+
+search_box = driver.find_element_by_css_selector('.topSearch__text')
 search_box.send_keys('プログラマー')
 sleep(1)
 search_box.submit()
